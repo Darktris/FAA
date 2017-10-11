6bc0cc09346ba6e72181f80978545be959adc3ce
aperera <pereradrian@gmail.com>
2017-10-11 16:38:52 +0200

    Falta a partir de naive bayes

--- clasificadores/clasificador_naive_bayes.py
+++ clasificadores/clasificador_naive_bayes.py
@@ -4,7 +4,7 @@ from clasificadores.clasificador import Clasificador
 class ClasificadorNaiveBayes(Clasificador):
     # TODO: implementar
     def entrenamiento(self, datostrain, atributosDiscretos, diccionario):
-        pass
+        numero_atributos = len(datostrain[0, :]) - 1
 
     # TODO: implementar
     def clasifica(self, datostest, atributosDiscretos, diccionario):
