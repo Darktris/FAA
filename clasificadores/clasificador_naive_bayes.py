2f03a2fd947d552f1aea5abea1cf3684057c8c8f
aperera <pereradrian@gmail.com>
2017-10-19 13:27:43 +0200

    Terminado el calculo de probabilidades en NB para atributos discretos. Falta implementar la clasificacion con el nuevo diseño por tablas. Falta implementar el calculo de probabilidades para atributos continuos. Falta hacer la comparativa con scikit. ...

--- clasificadores/clasificador_naive_bayes.py
+++ clasificadores/clasificador_naive_bayes.py
@@ -2,92 +2,72 @@ from clasificadores.clasificador import Clasificador
 import numpy  as np
 from scipy.stats import norm
 
+
 class ClasificadorNaiveBayes(Clasificador):
     laplace_smoothing = 0
     args = []
     posteriori_args = []
     posteriori = []
     clase = []
+
+    lista_tablas_probabilidades = []
+    prioris = []
+
     # Formula de Bayes: P(hip|datos) = P(datos|hip)*P(hip) = vero*priori
     def entrenamiento(self, datostrain, atributosDiscretos, diccionario):
 
-        numero_atributos = len(datostrain[0, :])
-        col_clases = np.asarray(datostrain[:, -1])
+        numero_atributos = len(diccionario) - 1
+        numero_clases = len(diccionario[-1])
 
-        self.priori = np.asarray(self.crearHistograma(col_clases))
-        self.clases = np.asarray(list(diccionario[-1].keys()))
-        self.posteriori = [None]*(numero_atributos-1)
-        self.posteriori_args = [None]*(numero_atributos-1)
+        self.lista_tablas_probabilidades = [None for _ in range(numero_atributos)]
+        self.prioris = [None] * numero_clases
 
-        # from IPython.core.debugger import Tracer;
-        # Tracer()()
+        for indice_clase, clase in enumerate(diccionario[-1].values()):
+            indices_casos_totales = datostrain[:, -1] == clase
+            cardinalidad_clase = indices_casos_totales.sum()
+            self.prioris[indice_clase] = cardinalidad_clase / len(datostrain[:, -1])
 
-        for indice in range(numero_atributos-1):
-            discreto = atributosDiscretos[indice]
-            columna = datostrain[:,indice]
+        for indice_atributo in range(numero_atributos):
+            # Una tabla por atributo
+            numero_valores_atributo = len(diccionario[indice_atributo].keys())
+            self.lista_tablas_probabilidades[indice_atributo] = np.ndarray(shape=(numero_valores_atributo, numero_clases))
 
-            self.posteriori_args[indice] = {}
-            self.posteriori[indice] = {}
+            if atributosDiscretos[indice_atributo]:
+                # Caso discreto
 
-            #print(self.clases)
-            if discreto:
-                for i_clase in range(len(self.clases)):
-                    clase = self.clases[i_clase]
-                    self.posteriori_args[indice][clase] = {}
-
-                    attribute_filtered = [elem for index, elem in enumerate(columna) if (col_clases[index] == clase)]
-                    hist = np.asarray(self.crearHistograma(attribute_filtered))
-
-                    self.posteriori_args[indice][clase]['hist'] = self.priori[i_clase]*hist
-
-                    # from IPython.core.debugger import Tracer; Tracer()()
-                    self.posteriori[indice][clase] = self.posterioriDiscreto
+                for indice_valor, valor in enumerate(diccionario[indice_atributo].values()):
+                    indices_casos_favorables = (datostrain[:, indice_atributo] == valor) & (datostrain[:, -1] == clase)
+                    numero_casos_favorables = indices_casos_favorables.sum()
+                    self.lista_tablas_probabilidades[indice_atributo][indice_valor][
+                        indice_clase] = numero_casos_favorables / cardinalidad_clase
             else:
-                self.posteriori_args[indice][clase] = {}
-                self.posteriori_args[indice][clase]['mu'] = np.mean(columna)
-                self.posteriori_args[indice][clase]['sigma'] = np.var(columna)
-                self.posteriori_args[indice][clase]['priori'] = self.priori[clase==self.clases]
-                self.posteriori[indice][clase] = self.posterioriContinuo
-    
-    def posterioriDiscreto(self, x, argumentos):
-        hist = argumentos['hist']
-        index = int(x)
-        if index in hist:
-            return float(hist[int(x)])
-        else:
-            return 0
-    
-    def posterioriContinuo(self, x, argumentos):
-        mu = argumentos['mu']
-        sigma = argumentos['sigma']
-        priori = argumentos['priori']
-        return float(norm.pdf(x, mu, sigma)*priori)
-    
-    def crearHistograma(self, x):
-
-        hist = np.ndarray(shape=(len(x),1))
-        bins = sorted(np.unique(x))
-        total = len(x)
-
-        for index, bin in enumerate(bins):
-            hist[index] = (x == bin).sum() / total
-
-        return hist
+                # TODO: Caso continuo
+                pass
 
 
-    def clasifica(self, datostest, atributosDiscretos, diccionario):
-        resultado = []
-        for record in datostest:
-            prob = {}
-            for clase in self.clases:
-                prob[clase] = 1.0
-                for fieldIndex in range(len(record)):
-                    prob[clase] *= self.posteriori[fieldIndex][clase](record[fieldIndex], self.posteriori_args[fieldIndex][clase])
+def posterioriContinuo(self, x, argumentos):
+    mu = argumentos['mu']
+    sigma = argumentos['sigma']
+    priori = argumentos['priori']
+    return float(norm.pdf(x, mu, sigma) * priori)
 
 
-            clase = np.argmax(prob)
-            resultado.append(clase)
+def clasifica(self, datostest, atributosDiscretos, diccionario):
+    #TODO: Terminar
+    for fila in datostest:
+        for indice_atributo, _ in enumerate(fila[:-1]):
 
-        return resultado
-                
-            
+
+
+    for record in datostest:
+        prob = {}
+        for clase in self.clases:
+            prob[clase] = 1.0
+            for fieldIndex in range(len(record)):
+                prob[clase] *= self.posteriori[fieldIndex][clase](record[fieldIndex],
+                                                                  self.posteriori_args[fieldIndex][clase])
+
+        clase = np.argmax(prob)
+        resultado.append(clase)
+
+    return resultado
