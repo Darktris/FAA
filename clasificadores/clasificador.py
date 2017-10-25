02c6aff3a9199362c47f65a93834cf3d03ebcc56
e298729 <kjaslkjnsa@lkadslk.com>
2017-10-25 20:07:07 +0200

    :c

--- clasificadores/clasificador.py
+++ clasificadores/clasificador.py
@@ -1,72 +1,55 @@
-from abc import ABCMeta,abstractmethod
+from abc import ABCMeta, abstractmethod
 import numpy as np
 
+
 class Clasificador(object):
-  
-  # Clase abstracta
-  __metaclass__ = ABCMeta
-  particiones = []
-  errores = []
+    # Clase abstracta
+    __metaclass__ = ABCMeta
+    particiones = []
+    errores = []
 
-  # Metodos abstractos que se implementan en casa clasificador concreto
-  @abstractmethod
-  # TODO: esta funcion deben ser implementadas en cada clasificador concreto
-  # datosTrain: matriz numpy con los datos de entrenamiento
-  # atributosDiscretos: array bool con la indicatriz de los atributos nominales
-  # diccionario: array de diccionarios de la estructura Datos utilizados para la codificacion
-  # de variables discretas
-  def entrenamiento(self,datosTrain,atributosDiscretos,diccionario):
-    pass
-  
-  
-  @abstractmethod
-  # TODO: esta funcion deben ser implementadas en cada clasificador concreto
-  # devuelve un numpy array con las predicciones
-  def clasifica(self,datosTest,atributosDiscretos,diccionario):
-    pass
-  
-  
-  # Obtiene el numero de aciertos y errores para calcular la tasa de fallo
-  # TODO: implementar
-  def error(self,datos,pred):
-    # Aqui se compara la prediccion (pred) con las clases reales y se calcula el error    
-    #prediccion = self.clasifica(datos, )
-    return float(np.sum(prediccion != pred))/len(pred)
-    
-    
-    
-    
-  # Realiza una clasificacion utilizando una estrategia de particionado determinada
-  # TODO: implementar esta funcion
-  def validacion(self, particionado, dataset, seed=None):
-       
-    # Creamos las particiones siguiendo la estrategia llamando a particionado.creaParticiones
-    # - Para validacion cruzada: en el bucle hasta nv entrenamos el clasificador con la particion de train i
-    # y obtenemos el error en la particion de test i
-    # - Para validacion simple (hold-out): entrenamos el clasificador con la particion de train
-    # y obtenemos el error en la particion test
-    self.particiones = particionado.creaParticiones(dataset.datos)
-    self.errores = []
-    for particion in self.particiones:
-        entrenamiento = dataset.extraeDatosTrain(particion.indicesTrain)
-        validacion = dataset.extraeDatosTest(particion.indicesTrain)
-        pred = validacion[:, -1]
-        attr = validacion[:,:-1]
-        self.entrenamiento(entrenamiento, dataset.nominalAtributos, dataset.diccionarios)
-        prediccion = self.clasifica(attr, dataset.nominalAtributos, dataset.diccionarios)
-        error = float(np.sum(prediccion != pred))/len(pred)
-        self.errores.append(error)
-		
+    # Metodos abstractos que se implementan en casa clasificador concreto
+    @abstractmethod
+    # datosTrain: matriz numpy con los datos de entrenamiento
+    # atributosDiscretos: array bool con la indicatriz de los atributos nominales
+    # diccionario: array de diccionarios de la estructura Datos utilizados para la codificacion
+    # de variables discretas
+    def entrenamiento(self, datosTrain, atributosDiscretos, diccionario):
+        pass
 
-       
-  
+    @abstractmethod
+    # devuelve un numpy array con las predicciones
+    def clasifica(self, datosTest, atributosDiscretos, diccionario):
+        pass
+
+
+    # Realiza una clasificacion utilizando una estrategia de particionado determinada
+    def validacion(self, particionado, dataset, seed=None):
+        # Creamos las particiones siguiendo la estrategia llamando a particionado.creaParticiones
+        # - Para validacion cruzada: en el bucle hasta nv entrenamos el clasificador con la particion de train i
+        # y obtenemos el error en la particion de test i
+        # - Para validacion simple (hold-out): entrenamos el clasificador con la particion de train
+        # y obtenemos el error en la particion test
+        self.particiones = particionado.creaParticiones(dataset.datos)
+
+        self.errores = []
+        predicciones = []
+
+        for particion in self.particiones:
+            entrenamiento = dataset.extraeDatosTrain(particion.indicesTrain)
+            _validacion = dataset.extraeDatosTest(particion.indicesTrain)
+            y_test = _validacion[:, -1]
+            x_test = _validacion[:, :-1]
+
+            self.entrenamiento(entrenamiento, dataset.nominalAtributos, dataset.diccionarios)
+            prediccion = self.clasifica(x_test, dataset.nominalAtributos, dataset.diccionarios)
+
+            error = float(np.sum(prediccion != y_test)) / len(y_test)
+
+            self.errores.append(error)
+
+            predicciones.append(prediccion)
+
+        return predicciones
+
 ##############################################################################
-
-    
-    
-
-
-
-
-
-  
--- clasificadores/clasificador_naive_bayes.py
+++ clasificadores/clasificador_naive_bayes.py
@@ -4,7 +4,7 @@ from scipy.stats import norm
 
 
 class ClasificadorNaiveBayes(Clasificador):
-    laplace_smoothing = 0
+    laplace_smoothing = 0.0
     args = []
     posteriori_args = []
     posteriori = []
@@ -13,10 +13,10 @@ class ClasificadorNaiveBayes(Clasificador):
     lista_tablas_probabilidades = []
     prioris = []
     parametros_normales = []
-    
+
     # Formula de Bayes: P(hip|datos) = P(datos|hip)*P(hip) = vero*priori
     def entrenamiento(self, datostrain, atributosDiscretos, diccionario):
-        
+
         numero_atributos = len(diccionario) - 1
         numero_clases = len(diccionario[-1])
 
@@ -24,45 +24,55 @@ class ClasificadorNaiveBayes(Clasificador):
         self.lista_tablas_probabilidades = [None for _ in range(numero_atributos)]
         self.prioris = [None] * numero_clases
         self.parametros_normales = [{'mu': 0, 'sigma': 0}] * numero_atributos
-        # Para cada clase
-        for indice_clase, clase in enumerate(diccionario[-1].values()):
+        cardinalidades_clase = np.ndarray(shape=(len(diccionario[-1]),))
 
+        for indice_clase, clase in enumerate(diccionario[-1].values()):
             # Casos totales = datos en los que la clase coincida
             indices_casos_totales = datostrain[:, -1] == clase
             # Numero de casos totales de esa clase
-            cardinalidad_clase = float(indices_casos_totales.sum())
-            
-            # Priori: datos de esa clase / datos totales
-            self.prioris[indice_clase] = (cardinalidad_clase) / (len(datostrain[:, -1]))
+            cardinalidades_clase[indice_clase] = float(indices_casos_totales.sum())
 
-            # Para cada atributo
-            for indice_atributo in range(numero_atributos):
-                # Una tabla por atributo
-                numero_valores_atributo = len(diccionario[indice_atributo].keys())
-                # Diccionario que contiene para todo valor de ese atributo, un 0
-                dictionary = [(value , 0) for value in diccionario[indice_atributo].values()]
-                # La tabla contiene en ese atributo contiene el diccionario tantas veces como el numero de clases
-                self.lista_tablas_probabilidades[indice_atributo] = [dict(dictionary)] * numero_clases
-                # Si es discreto
-                if atributosDiscretos[indice_atributo]:
-                    # Para cada valor del atributo
-                    for valor_atributo in diccionario[indice_atributo].values():
+            # Priori: datos de esa clase / datos totales
+            self.prioris[indice_clase] = (cardinalidades_clase[indice_clase]) / (len(datostrain[:, -1]))
+
+        print(cardinalidades_clase/sum(cardinalidades_clase) )
+
+        ## Hasta qquie bien TODO
+
+        # Para cada atributo
+        for indice_atributo in range(numero_atributos):
+            # Una tabla por atributo
+            numero_valores_atributo = len(diccionario[indice_atributo].keys())
+            # Diccionario que contiene para to_do valor de ese atributo, un 0
+            # La tabla contiene en ese atributo contiene el diccionario tantas veces como el numero de clases
+
+            self.lista_tablas_probabilidades[indice_atributo] = [
+                dict([(value, 0) for value in diccionario[indice_atributo].values()]) for
+                _ in range(numero_clases)]
+
+            # Si es discreto
+            if atributosDiscretos[indice_atributo]:
+                # Para cada valor del atributo
+                for valor_atributo in diccionario[indice_atributo].values():
+                    for indice_clase, clase in enumerate(diccionario[-1].values()):
                         # indices casos favorables son los datos en los que el valor del atributo esta y la clase es la deseada
-                        indices_casos_favorables = (datostrain[:, indice_atributo] == valor_atributo) & (datostrain[:, -1] == clase)
+                        indices_casos_favorables = (datostrain[:, indice_atributo] == valor_atributo) & (
+                            datostrain[:, -1] == clase)
                         # Numero de casos favorables
                         numero_casos_favorables = float(indices_casos_favorables.sum())
                         # Verosimilitud es numero de casos favorables entre todos los de la clase
-                        self.lista_tablas_probabilidades[indice_atributo][indice_clase][valor_atributo] = (numero_casos_favorables+self.laplace_smoothing) / (cardinalidad_clase + self.laplace_smoothing*numero_valores_atributo*numero_atributos)
-                        #print(numero_casos_favorables, cardinalidad_clase)
-                        #print('attr:', indice_atributo, 'clase:', indice_clase, 'valor_attr:',valor_atributo, self.lista_tablas_probabilidades[indice_atributo][indice_clase][valor_atributo])
+                        probabilidad = (numero_casos_favorables + self.laplace_smoothing) / (
+                            cardinalidades_clase[
+                                indice_clase] + self.laplace_smoothing * numero_valores_atributo * numero_atributos)
 
+                        self.lista_tablas_probabilidades[indice_atributo][indice_clase][valor_atributo] = probabilidad
+                print(self.lista_tablas_probabilidades[indice_atributo][indice_clase])
 
-                else:
-                    # Calculo media 
-                    self.parametros_normales[indice_atributo]['mu'] = np.mean(datostrain[:, indice_atributo])
-                    # Calculo varianza
-                    self.parametros_normales[indice_atributo]['var'] = np.var(datostrain[:, indice_atributo])
-
+            else:
+                # Calculo media
+                self.parametros_normales[indice_atributo]['mu'] = np.mean(datostrain[:, indice_atributo])
+                # Calculo varianza
+                self.parametros_normales[indice_atributo]['var'] = np.var(datostrain[:, indice_atributo])
 
     def posterioriContinuo(self, x, argumentos):
         mu = argumentos['mu']
@@ -70,36 +80,33 @@ class ClasificadorNaiveBayes(Clasificador):
         priori = argumentos['priori']
         return float(norm.pdf(x, mu, sigma) * priori)
 
-
     def clasifica(self, datostest, atributosDiscretos, diccionario):
-        #TODO: Terminar
-
         # print(len(self.lista_tablas_probabilidades))
         # for tabla in self.lista_tablas_probabilidades:
         #     print(tabla)
-        
+
         # Inicializamos con las probabilidades a priori para cada clase
-        probabilidades = np.asarray([[ self.prioris[indice_clase] for indice_clase in diccionario[-1].values() ]] * len(datostest))
+        probabilidades = np.asarray([self.prioris] * len(datostest[:,-1]))
+
+        print("-------")
         # Para cada registro del dataset de clasificacion
         for indice_fila, fila in enumerate(datostest):
             # Para cada atributo del registro
             for indice_atributo, valor_atributo in enumerate(fila[:-1]):
                 # Para cada clase
                 for indice_clase in diccionario[-1].values():
+
                     if atributosDiscretos[indice_atributo]:
-                        probabilidades[indice_fila][indice_clase]*=self.lista_tablas_probabilidades[indice_atributo][indice_clase][valor_atributo]
-                        #print('fila:', indice_fila, 'attr:', indice_atributo, 'clase:', indice_clase, 'valor_attr:',valor_atributo, self.lista_tablas_probabilidades[indice_atributo][indice_clase])
+                        probabilidades[indice_fila][indice_clase] *= \
+                            self.lista_tablas_probabilidades[indice_atributo][indice_clase][valor_atributo]
                     else:
                         mu = self.parametros_normales[indice_atributo]['mu']
                         sigma = self.parametros_normales[indice_atributo]['sigma']
                         if sigma != 0:
-                            probabilidades[indice_fila][indice_clase]*=float(norm.pdf(valor_atributo, mu, sigma))
+                            probabilidades[indice_fila][indice_clase] *= float(norm.pdf(valor_atributo, mu, sigma))
+                #print(indice_fila,":",probabilidades[indice_fila])
+        #print(probabilidades)
+
         prediccion = np.argmax(probabilidades, axis=1)
-        #for probabilidad in probabilidades:
-        #    print(probabilidad)
-        
-        for _i in range(len(prediccion)):
-            print(prediccion[_i], probabilidades[_i])
-        #print('argmax', np.argmax(probabilidades, axis=1), probabilidades)
 
         return prediccion
